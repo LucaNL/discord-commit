fb277947764cf1be6304b801c23c1582af50a299
LucaNL <lucanl.beheer@gmail.com>
2024-02-22 15:38:49 +0100

    asd

--- index.js
+++ index.js
@@ -1,45 +1,54 @@
-const axios = require('axios')
+const axios = require("axios");
 
-const core = require('@actions/core')
-const github = require('@actions/github')
+const core = require("@actions/core");
+const github = require("@actions/github");
 
-const webhook = core.getInput('webhook')
+const webhook = core.getInput("webhook");
 
 if (!/https:\/\/discord(app|)\.com\/api\/webhooks\/\d+?\/.+/i.exec(webhook)) {
-  core.setFailed('The given discord webhook url is invalid. Please ensure you give a **full** url that start with "https://discordapp.com/api/webhooks"')
+  core.setFailed(
+    'The given discord webhook url is invalid. Please ensure you give a **full** url that start with "https://discordapp.com/api/webhooks"'
+  );
 }
 
-const shortSha = (i) => i.substr(0, 6)
+const shortSha = (i) => i.substr(0, 6);
 
-const escapeMd = (str) => str.replace(/([\[\]\\`\(\)])/g, '\\$1')
+const escapeMd = (str) => str.replace(/([\[\]\\`\(\)])/g, "\\$1");
 
-const { payload: githubPayload } = github.context
+const { payload: githubPayload } = github.context;
 
-const commits = githubPayload.commits.map(i => ` - [\`[${shortSha(i.id)}]\`](${i.url}) ${escapeMd(i.message)} - by ${i.author.name}`)
+const commits = githubPayload.commits.map(
+  (i) =>
+    ` - [\`[${shortSha(i.id)}]\`](${i.url}) ${escapeMd(i.message)} - by ${
+      i.author.name
+    }`
+);
 
 if (!commits.length) {
-  return
+  return;
 }
 
-const beforeSha = githubPayload.before
-const afterSha = githubPayload.after
-const compareUrl = `${githubPayload.repository.url}/compare/${beforeSha}...${afterSha}`
+const beforeSha = githubPayload.before;
+const afterSha = githubPayload.after;
+const compareUrl = `${githubPayload.repository.url}/compare/${beforeSha}...${afterSha}`;
 
 const payload = {
-  content: '',
+  content: "",
   embeds: [
     {
-      title: core.getInput('message-title') || 'Commits received',
-      description: `[\`\[${shortSha(beforeSha)}...${shortSha(afterSha)}\]\`](${compareUrl})\n${commits.join('\n')}`
-    }
-  ]
-}
+      title: "Commits received",
+      description: `[\`\[${shortSha(beforeSha)}...${shortSha(
+        afterSha
+      )}\]\`](${compareUrl})\n${commits.join("\n")}`,
+    },
+  ],
+};
 
 axios
   .post(webhook, payload)
   .then((res) => {
-    core.setOutput('result', 'Webhook sent')
+    core.setOutput("result", "Webhook sent");
   })
   .catch((err) => {
-    core.setFailed(`Post to webhook failed, ${err}`)
-  })
+    core.setFailed(`Post to webhook failed, ${err}`);
+  });
